31f75f91c2d7a4dd24bce55db339750658480271
RainBowT0506 <rainbowt0506@gmail.com>
2024-04-17 15:42:58 +0800

    Generate Simple pdf file

---
+++ app/Http/Controllers/PdfController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class PdfController extends Controller
+{
+    public function generatePdf(){
+        $pdf = App::make('dompdf.wrapper');
+        //  通過修改 HTML 內容，可以在生成的 PDF 中包含不同的元素，例如 `<h1>`、 `<p>`標籤和段落。
+        $pdf->loadHTML('<p>Generate Simple pdf file</p>');
+        return $pdf->stream();
+    }
+
+    public function downloadPdf(){
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadHTML('<h1>Generate Simple pdf file</h1>');
+        return $pdf->download();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PdfController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('generate-pdf',[PdfController::class,'generatePdf'])->name('generate-pdf');
+
+Route::get('download-pdf',[PdfController::class,'downloadPdf'])->name('download-pdf');
